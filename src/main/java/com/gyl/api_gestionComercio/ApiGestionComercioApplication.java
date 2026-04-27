67cd5c8bf7f5512aa29ce71edee6a0addc2bec8f
Lucas Federico Lopez <lopezlucasdev@gmail.com>
2026-04-27 18:44:08 -0300

    APIRest gestion de Comercios V0.0

---
+++ src/main/java/com/gyl/api_gestionComercio/ApiGestionComercioApplication.java
@@ -0,0 +1,13 @@
+package com.gyl.api_gestionComercio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGestionComercioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGestionComercioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyl/api_gestionComercio/ApiGestionComercioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyl.api_gestionComercio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGestionComercioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
